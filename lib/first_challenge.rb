0828595a82b50808577c8a9e9286d9cb9a4c1972
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-07 14:30:22 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -20,5 +20,10 @@ contacts.each do |person, data|
            value.delete_if do |flavor|
              flavor == "strawberry"
            end
+         end
+       end
+     end
+   end
+ end
   contacts
 end
