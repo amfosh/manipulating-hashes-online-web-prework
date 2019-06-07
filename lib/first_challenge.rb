6990d84dd0385a3e5afa9200417daf8653290c13
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-07 14:17:56 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 contacts.each do |person, data|
   data.each do |attribute, value|
     if attribute == :favorite_icecream_flavors
-      
+      .delete_if 
 
   #remember to return your newly altered contacts hash!
   contacts
