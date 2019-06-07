8c8e41ac6b62da154e62f3f76c5fd54f114c1682
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-07 14:32:20 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,17 +14,15 @@ def first_challenge
   }
 
 contacts.each do |person, data|
-     if person == "Freddy Mercury"
-       data.each do |attribute, value|
-         if attribute == :favorite_icecream_flavors
-           value.delete_if do |flavor|
-             flavor == "strawberry"
-           end
+  if person == "Freddy Mercury"
+    data.each do |attribute, value|
+      if attribute == :favorite_icecream_flavors
+        value.delete_if do |flavor|
+          flavor == "strawberry"
          end
        end
      end
    end
  end
-  contacts
-end
+contacts
 end
